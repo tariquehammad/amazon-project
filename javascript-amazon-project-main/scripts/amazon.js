0e369fb8515aa60d1e9eccea3731636dc203cd9a
tariquehammad <mo.hammadtarique1963@gmail.com>
2024-08-01 00:48:47 +0530

    make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -57,7 +57,7 @@ products.forEach((product) => {
 
 });
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
@@ -79,6 +79,14 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+        console.log(cartQuantity);
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     });
   });
