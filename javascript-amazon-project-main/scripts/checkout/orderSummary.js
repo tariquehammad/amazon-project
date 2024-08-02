04f94cc5f1591165ad0ffc2a79c3a748dd91f5da
tariquehammad <mo.hammadtarique1963@gmail.com>
2024-08-03 00:29:14 +0530

    Make payment summary interactive

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -3,12 +3,8 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency} from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
-
-const today =  dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D');
-console.log(deliveryDate);
 
 
 export function renderOrderSummary(){
@@ -119,9 +115,11 @@ export function renderOrderSummary(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
+
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         console.log(container);
         container.remove();
+        renderPaymentSummary();
       })
       
     });
@@ -131,6 +129,7 @@ export function renderOrderSummary(){
         const {productId,deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 
